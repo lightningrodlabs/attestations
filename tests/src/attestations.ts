82e3adf32b3e547077868e895d48e330f44ad9ec
Eric Harris-Braun <eric@harris-braun.com>
2022-07-07 16:46:50 -0400

    WIP getting tests to work with new tryorama

--- tests/src/attestations.ts
+++ tests/src/attestations.ts
@@ -1,45 +1,43 @@
-import { Orchestrator, Config, InstallAgentsHapps } from "@holochain/tryorama";
+import { ActionHash, DnaSource } from "@holochain/client";
+import { pause, runScenario, Scenario  } from "@holochain/tryorama";
 import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 import * as _ from "lodash";
 import {Attestation, AttestationOutput} from "../../ui/lib/src/types"
+import test from "tape-promise/tape.js";
+import {EntryHashB64, AgentPubKeyB64} from "@holochain-open-dev/core-types";
 
-import {
-  RETRY_DELAY,
-  RETRY_COUNT,
-  localConductorConfig,
-  networkedConductorConfig,
-  installAgents,
-  awaitIntegration,
-  delay,
-} from "./common";
 import { Base64 } from "js-base64";
 
-function serializeHash(hash: Uint8Array): string {
+function serializeHash(hash: Uint8Array): AgentPubKeyB64 {
   return `u${Base64.fromUint8Array(hash, true)}`;
 }
 
-function checkAttestations(t, attestationObject: AttestationOutput, attestation: Attestation, hash: string, attesters: Array<string>) {
+function checkAttestations(t, attestationObject: AttestationOutput, attestation: Attestation, hash: EntryHashB64, attesters: Array<AgentPubKeyB64>) {
   t.equal(attestationObject.hash, hash)
   t.deepEqual(attestationObject.content, attestation)
   attestationObject.attesters.forEach((element,i) => {
     t.equal(attesters[i], element.author)
   });
 }
-export default async (orchestrator) => {
-  orchestrator.registerScenario("attestations basic tests", async (s, t) => {
-    // Declare two players using the previously specified config, nicknaming them "alice" and "bob"
-    // note that the first argument to players is just an array conductor configs that that will
-    // be used to spin up the conductor processes which are returned in a matching array.
-    const [a_and_b_conductor] = await s.players([localConductorConfig]);
 
-    // install your happs into the conductors and destructuring the returned happ data using the same
-    // array structure as you created in your installation array.
-    let [alice_attestations_happ, bobbo_attestations_happ] =
-      await installAgents(a_and_b_conductor, ["alice", "bobbo"]);
-    const [alice_attestations] = alice_attestations_happ.cells;
-    const [bobbo_attestations] = bobbo_attestations_happ.cells;
-    const boboAgentKey = serializeHash(bobbo_attestations.cellId[1]);
-    const aliceAgentKey = serializeHash(alice_attestations.cellId[1]);
+test("attestations basic tests", async (t) => {
+  await runScenario(async (scenario: Scenario) => {
+
+
+    export const dnaPath = path.join(__dirname, "../../dna/workdir/dna/attestations.dna")
+    const dnas: DnaSource[] = [{ path: dnaPath }];
+    const [alice, bobbo] = await scenario.addPlayersWithHapps([dnas, dnas]);
+    await scenario.shareAllAgents();
+
+
+    const [alice_attestations] = alice.cells;
+    const [bobbo_attestations] = bobbo.cells;
+    const boboAgentKey = serializeHash(bobbo.agentPubKey);
+    const aliceAgentKey = serializeHash(alice.agentPubKey);
 
     // Create an attestation
     let attestation1 = {
@@ -47,82 +45,83 @@ export default async (orchestrator) => {
       about: boboAgentKey,
     };
 
-    const attestation1_hash = await alice_attestations.call(
-      "hc_zome_attestations",
-      "create_attestation",
-      attestation1
+    const attestation1_hash: EntryHashB64 = await alice_attestations.callZome( {
+      zome_name: "hc_zome_attestations",
+      fn_name: "create_attestation",
+      payload: attestation1
+    }
     );
     t.ok(attestation1_hash);
     console.log("attestation1_hash", attestation1_hash);
 
-    let attestations = await alice_attestations.call(
-      "hc_zome_attestations",
-      "get_my_attestations",
-      null
-    );
+    let attestations : Array<AttestationOutput> = await alice_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "get_my_attestations",
+      payload: null
+    });
+    console.log(attestations);
+    checkAttestations(t, attestations[0], attestation1, attestation1_hash, [aliceAgentKey])
+
+    attestations = await alice_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "get_attestations",
+      payload: {of: boboAgentKey}
+    });
     console.log(attestations);
     checkAttestations(t,attestations[0], attestation1, attestation1_hash, [aliceAgentKey])
 
-    attestations = await alice_attestations.call(
-      "hc_zome_attestations",
-      "get_attestations",
-      {of: boboAgentKey}
-    );
+    attestations = await alice_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "get_attestations",
+      payload: {by: aliceAgentKey}
+    });
     console.log(attestations);
+    t.is(attestations.length,1)
     checkAttestations(t,attestations[0], attestation1, attestation1_hash, [aliceAgentKey])
 
-    attestations = await alice_attestations.call(
-      "hc_zome_attestations",
-      "get_attestations",
-      {by: aliceAgentKey}
+    attestations = await alice_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "get_attestations",
+      payload: {by: aliceAgentKey,
+       of: boboAgentKey}}
     );
     console.log(attestations);
     t.is(attestations.length,1)
     checkAttestations(t,attestations[0], attestation1, attestation1_hash, [aliceAgentKey])
 
-    attestations = await alice_attestations.call(
-      "hc_zome_attestations",
-      "get_attestations",
-      {by: aliceAgentKey,
-       of: boboAgentKey}
-    );
-    console.log(attestations);
-    t.is(attestations.length,1)
-    checkAttestations(t,attestations[0], attestation1, attestation1_hash, [aliceAgentKey])
-
-    attestations = await alice_attestations.call(
-      "hc_zome_attestations",
-      "get_attestations",
-      {content: "Cool Person"}
-    );
+    attestations = await alice_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "get_attestations",
+      payload: {content: "Cool Person"}
+    });
     console.log("Should be bobbo", attestations);
     checkAttestations(t,attestations[0], attestation1, attestation1_hash, [aliceAgentKey])
 
 
-    attestations = await alice_attestations.call(
-      "hc_zome_attestations",
-      "get_attestations",
-      {agent: aliceAgentKey}
-    );
+    attestations = await alice_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "get_attestations",
+      payload: {agent: aliceAgentKey}
+    });
     console.log("Should be nothing", attestations);
     t.deepEqual(attestations, []);
 
-    const attestation2_hash = await bobbo_attestations.call(
-      "hc_zome_attestations",
-      "create_attestation",
-      attestation1
-    );
+    const attestation2_hash = await bobbo_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "create_attestation",
+      payload: attestation1
+    });
     t.ok(attestation1_hash);
     t.equal(attestation2_hash, attestation1_hash)
-    attestations = await bobbo_attestations.call(
-      "hc_zome_attestations",
-      "get_my_attestations",
+    attestations = await bobbo_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "get_my_attestationspayload: ",}
     );
     t.equal(attestations[0].attesters.length, 2);
 
-    attestations = await alice_attestations.call(
-      "hc_zome_attestations",
-      "get_my_attestations",
+    attestations = await alice_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "get_my_attestationspayload: ",}
     );
     t.equal(attestations[0].attesters.length, 2);
 
@@ -131,32 +130,32 @@ export default async (orchestrator) => {
       content: "Cool Person",
       about: aliceAgentKey,
     };
-    const attestation3_hash = await bobbo_attestations.call(
-      "hc_zome_attestations",
-      "create_attestation",
-      attestation2
-    );
+    const attestation3_hash = await bobbo_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "create_attestation",
+      payload: attestation2
+    });
     t.ok(attestation3_hash);
     console.log("attestation3_hash", attestation1_hash);
 
-    attestations = await bobbo_attestations.call(
-      "hc_zome_attestations",
-      "get_attestations",
-      //{content: "Cool Person"}
+    attestations = await bobbo_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "get_attestations",
+      payload: //{content: "Cool Person"}
       {by: boboAgentKey,
        of: boboAgentKey
-      }
+      }}
     );
     console.log(attestations);
     t.is(attestations.length,2)
 
-    let nonce = await alice_attestations.call(
-      "hc_zome_attestations",
-      "create_nonce",
-      {with: boboAgentKey, note: "bobbo"},
-    );
+    let nonce = await alice_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "create_nonce",
+      payload: {with: boboAgentKey, note: "bobbo"},
+    });
     console.log("nonce:", nonce)
-
+/*
     a_and_b_conductor.setSignalHandler((signal) => {
       console.log("Received Signal:", signal);
       t.deepEqual(signal.data.payload, {
@@ -165,21 +164,21 @@ export default async (orchestrator) => {
       });
     });
 
-    let result = await bobbo_attestations.call(
-      "hc_zome_attestations",
-      "fulfill_nonce",
-      {with: aliceAgentKey,
+    let result = await bobbo_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "fulfill_nonce",
+      payload: {with: aliceAgentKey,
        nonce},
-    );
+      });
     console.log("result:", result)
-    attestations = await bobbo_attestations.call(
-      "hc_zome_attestations",
-      "get_attestations",
-      {by: boboAgentKey}
-    );
+    attestations = await bobbo_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "get_attestations",
+      payload: {by: boboAgentKey}
+    });
     console.log(attestations);
     t.is(attestations.length,3)
-
+*/
   });
 
 };
--- tests/src/index.ts
+++ tests/src/index.ts
@@ -1,11 +1,184 @@
-import { Orchestrator } from "@holochain/tryorama";
-import attestations from "./attestations";
+import { ActionHash, DnaSource } from "@holochain/client";
+import { pause, runScenario, Scenario  } from "@holochain/tryorama";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dnaPath = path.join(__dirname, "../../dna/workdir/dna/attestations.dna")
 
-let orchestrator = new Orchestrator();
-attestations(orchestrator);
-orchestrator.run();
+import * as _ from "lodash";
+import {Attestation, AttestationOutput} from "../../ui/lib/src/types"
+import test from "tape-promise/tape.js";
+import {EntryHashB64, AgentPubKeyB64} from "@holochain-open-dev/core-types";
+
+import { Base64 } from "js-base64";
+import { delay } from "lodash";
+
+function serializeHash(hash: Uint8Array): AgentPubKeyB64 {
+  return `u${Base64.fromUint8Array(hash, true)}`;
+}
+
+function checkAttestations(t, attestationObject: AttestationOutput, attestation: Attestation, hash: EntryHashB64, attesters: Array<AgentPubKeyB64>) {
+  t.equal(attestationObject.hash, hash)
+  t.deepEqual(attestationObject.content, attestation)
+  attestationObject.attesters.forEach((element,i) => {
+    t.equal(attesters[i], element.author)
+  });
+}
+
+test("attestations basic tests", async (t) => {
+  await runScenario(async (scenario: Scenario) => {
+
+
+    const dnas: DnaSource[] = [{ path: dnaPath }];
+    const [alice, bobbo] = await scenario.addPlayersWithHapps([dnas, dnas]);
+    await scenario.shareAllAgents();
+
+
+    const [alice_attestations] = alice.cells;
+    const [bobbo_attestations] = bobbo.cells;
+    const boboAgentKey = serializeHash(bobbo.agentPubKey);
+    const aliceAgentKey = serializeHash(alice.agentPubKey);
+
+    // Create an attestation
+    let attestation1 = {
+      content: "Cool Person",
+      about: boboAgentKey,
+    };
+
+    const attestation1_hash: EntryHashB64 = await alice_attestations.callZome( {
+      zome_name: "hc_zome_attestations",
+      fn_name: "create_attestation",
+      payload: attestation1
+    }
+    );
+    t.ok(attestation1_hash);
+    console.log("attestation1_hash", attestation1_hash);
+
+    let attestations : Array<AttestationOutput> = await alice_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "get_my_attestations",
+      payload: null
+    });
+    console.log(attestations);
+    checkAttestations(t, attestations[0], attestation1, attestation1_hash, [aliceAgentKey])
+
+    attestations = await alice_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "get_attestations",
+      payload: {of: boboAgentKey}
+    });
+    console.log(attestations);
+    checkAttestations(t,attestations[0], attestation1, attestation1_hash, [aliceAgentKey])
+
+    attestations = await alice_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "get_attestations",
+      payload: {by: aliceAgentKey}
+    });
+    console.log(attestations);
+    t.is(attestations.length,1)
+    checkAttestations(t,attestations[0], attestation1, attestation1_hash, [aliceAgentKey])
+
+    attestations = await alice_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "get_attestations",
+      payload: {by: aliceAgentKey,
+       of: boboAgentKey}}
+    );
+    console.log(attestations);
+    t.is(attestations.length,1)
+    checkAttestations(t,attestations[0], attestation1, attestation1_hash, [aliceAgentKey])
+
+    attestations = await alice_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "get_attestations",
+      payload: {content: "Cool Person"}
+    });
+    console.log("Should be bobbo", attestations);
+    checkAttestations(t,attestations[0], attestation1, attestation1_hash, [aliceAgentKey])
+
+
+    attestations = await alice_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "get_attestations",
+      payload: {agent: aliceAgentKey}
+    });
+    console.log("Should be nothing", attestations);
+    t.deepEqual(attestations, []);
+
+    const attestation2_hash = await bobbo_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "create_attestation",
+      payload: attestation1
+    });
+    t.ok(attestation1_hash);
+    t.equal(attestation2_hash, attestation1_hash)
+    attestations = await bobbo_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "get_my_attestations",}
+    );
+    t.equal(attestations[0].attesters.length, 2);
+    await pause(500)
+    attestations = await alice_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "get_my_attestations",}
+    );
+    t.equal(attestations[0].attesters.length, 2);
+
+    // now have bobbo say Alice is a cool person
+    const attestation2 = {
+      content: "Cool Person",
+      about: aliceAgentKey,
+    };
+    const attestation3_hash = await bobbo_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "create_attestation",
+      payload: attestation2
+    });
+
+    t.ok(attestation3_hash);
+    console.log("attestation3_hash", attestation1_hash);
+
+    attestations = await bobbo_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "get_attestations",
+      payload: //{content: "Cool Person"}
+      {by: boboAgentKey,
+       of: boboAgentKey
+      }}
+    );
+    t.equal(attestations.length,2)
+
+    let nonce = await alice_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "create_nonce",
+      payload: {with: boboAgentKey, note: "bobbo"},
+    });
+    console.log("nonce:", nonce)
 /*
-orchestrator = new Orchestrator()
-require('./profile')(orchestrator)
-orchestrator.run()
+    a_and_b_conductor.setSignalHandler((signal) => {
+      console.log("Received Signal:", signal);
+      t.deepEqual(signal.data.payload, {
+        nonce: `${nonce}`,
+        from: boboAgentKey
+      });
+    });
+
+    let result = await bobbo_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "fulfill_nonce",
+      payload: {with: aliceAgentKey,
+       nonce},
+      });
+    console.log("result:", result)
+    attestations = await bobbo_attestations.callZome({
+      zome_name: "hc_zome_attestations",
+      fn_name: "get_attestations",
+      payload: {by: boboAgentKey}
+    });
+    console.log(attestations);
+    t.is(attestations.length,3)
 */
+  });
+});
